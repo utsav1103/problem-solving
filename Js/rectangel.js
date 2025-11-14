589672781d1069dbf5ec730dd8272e16788f7fc9
utsav-baghela <utsavu858@gmail.com>
2025-11-14 19:39:20 +0530

    testing area of rectangel with negative values and fixing it

--- Js/rectangel.js
+++ Js/rectangel.js
@@ -4,8 +4,16 @@ console.log("Hello world")
 //area of rectangle
 
 function area (l,b){
-    const areaofRectangle = l*b;
-    console.log(areaofRectangle)
-}
+    //making is non negative
+    if(l,b < 0) {
+        throw new Error("length and breath can not be negative")
+    }else{
+         const areaofRectangle = l*b;
+    console.log("Area of Reactangle is -" ,areaofRectangle)
+
+    }
+   }
 
 area(4,4)
+area(22,473)
+area(13,-2) //throwing new error 
