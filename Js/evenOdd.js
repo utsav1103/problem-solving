b2a31e50787b03000536d78947cef91d8b2f436d
utsav-baghela <utsavu858@gmail.com>
2025-11-15 20:24:40 +0530

    tesing even or odd

--- Js/evenOdd.js
+++ Js/evenOdd.js
@@ -1,11 +1,21 @@
 //if a number is even or odd
 
-function num (number){
-    if(number%2==0){
-        console.log(number,"is Even Number")
-    }else{
-        console.log(number,"is Odd Number")
-    }
+function num(number){
+    return number % 2 === 0 ? "even" : "odd"
+    // if(number%2==0){
+    //     console.log(number,"is Even Number")
+    // }else{
+    //     console.log(number,"is Odd Number")
+    // }
 }
 
-num(4)
+// num(4)
+// num(-4)
+// num(0)
+// num(-13)
+
+
+console.log(num(79)) ;
+console.log(num(-13)) ;
+console.log(num(10)) ;
+console.log(num(4)) ;
