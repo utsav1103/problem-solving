267f2fd627cc64f7310bc50977fa2fb8859d061a
utsav-baghela <utsavu858@gmail.com>
2025-11-15 20:15:28 +0530

    even or odd

---
+++ Js/evenOdd.js
@@ -0,0 +1,11 @@
+//if a number is even or odd
+
+function num (number){
+    if(number%2==0){
+        console.log(number,"is Even Number")
+    }else{
+        console.log(number,"is Odd Number")
+    }
+}
+
+num(4)
