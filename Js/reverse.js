5cb7b5182ef76ea297f305675067663b3e991411
utsav-baghela <utsavu858@gmail.com>
2025-11-17 22:10:16 +0530

    another method and testing

--- Js/reverse.js
+++ Js/reverse.js
@@ -1,14 +1,30 @@
 //reverse the string
 
-function reverseString(str){
+// function reverseString(str){
  
-    let reversed = '';
-    for(let i = str.length - 1; i>=0; i--){
-        reversed += str[i]
-    }
-    return reversed;
+//     let reversed = '';
+//     for(let i = str.length - 1; i>=0; i--){
+//         reversed += str[i]
+//     }
+//     return reversed;
 
-}
- const originalStr = "Mikasa";
- const reversedStr = reverseString(originalStr);
- console.log(reversedStr);
+// }
+//  const originalStr = "Mikasa";
+//  const reversedStr = reverseString(originalStr);
+//  console.log(reversedStr);
+
+
+ //another method
+
+ let str1 = 'Go to Gym'
+ let str2 = 'Hello Mikasa'
+ let str3 = 'Rocky Bhai'
+ let str4 = 'TOJI FUSIGIRO'
+ const reverse1 = str1.split('').reverse().join('');
+ console.log(reverse1);
+ const reverse2 = str2.split('').reverse().join('');
+ console.log(reverse2)
+ const reverse3 = str3.split('').reverse().join('');
+ console.log(reverse3)
+ const reverse4 = str4.split('').reverse().join('');
+ console.log(reverse4)
