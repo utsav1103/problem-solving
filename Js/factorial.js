13021e1be2401ff3d0e8ed55dd2a20771e0120d6
utsav-baghela <utsavu858@gmail.com>
2025-11-20 19:23:38 +0530

    factorial with recursion

--- Js/factorial.js
+++ Js/factorial.js
@@ -1,24 +1,40 @@
 // claculating the factorial
 
-function factorial(Num) {
+// function factorial(Num) {
 
-  if (typeof Num !== 'number' || Number.isNaN(Num)){
-    throw new Error("Num must be a valid number")
-  }
-  if(!Number.isInteger(Num)){
-    throw new Error("Num is not an integer")
-  }
+//   if (typeof Num !== 'number' || Number.isNaN(Num)){
+//     throw new Error("Num must be a valid number")
+//   }
+//   if(!Number.isInteger(Num)){
+//     throw new Error("Num is not an integer")
+//   }
 
+//   if(Num < 0){
+//     throw new Error ("Num is not a psoitive integer")
+//   }
+//   let result = 1;
+//   for (let i = 1; i <= Num; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
+
+// //console.log(factorial(13));
+// console.log(factorial('qweq'));
+// //console.log(factorial(-13));
+
+
+
+//++++++++====>>>>>> with REcursion
+
+function factorialWithRecursion(Num){
   if(Num < 0){
-    throw new Error ("Num is not a psoitive integer")
+    throw new Error('ONly possitive integers')
   }
-  let result = 1;
-  for (let i = 1; i <= Num; i++) {
-    result = result * i;
+  if(Num === 0 || Num === 1){
+    return 1;
   }
-  return result;
+  return Num * factorialWithRecursion(Num - 1);
 }
 
-//console.log(factorial(13));
-console.log(factorial('qweq'));
-//console.log(factorial(-13));
+console.log(factorialWithRecursion(4));
