d8995b2584569c54c5e303bb236e3c537534af32
utsav-baghela <utsavu858@gmail.com>
2025-11-20 19:15:23 +0530

    testing factorial code

--- Js/factorial.js
+++ Js/factorial.js
@@ -1,6 +1,17 @@
 // claculating the factorial
 
 function factorial(Num) {
+
+  if (typeof Num !== 'number' || Number.isNaN(Num)){
+    throw new Error("Num must be a valid number")
+  }
+  if(!Number.isInteger(Num)){
+    throw new Error("Num is not an integer")
+  }
+
+  if(Num < 0){
+    throw new Error ("Num is not a psoitive integer")
+  }
   let result = 1;
   for (let i = 1; i <= Num; i++) {
     result = result * i;
@@ -8,4 +19,6 @@ function factorial(Num) {
   return result;
 }
 
-console.log(factorial(3));
+//console.log(factorial(13));
+console.log(factorial('qweq'));
+//console.log(factorial(-13));
